799c4fcd78baac72c4a0e1774894be1e745393db
Stan Effy <holydonk@gmail.com>
2022-11-02 21:36:15 +0200

    create guessing game

---
+++ guessing_game/target/debug/deps/rand_chacha-23a354d385b5dbf8.d
@@ -0,0 +1,9 @@
+/run/media/stan/D/projects/rust/guessing_game/target/debug/deps/rand_chacha-23a354d385b5dbf8.rmeta: /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/run/media/stan/D/projects/rust/guessing_game/target/debug/deps/librand_chacha-23a354d385b5dbf8.rlib: /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/run/media/stan/D/projects/rust/guessing_game/target/debug/deps/rand_chacha-23a354d385b5dbf8.d: /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs /home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs
+
+/home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/lib.rs:
+/home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/chacha.rs:
+/home/stan/.cargo/registry/src/github.com-1ecc6299db9ec823/rand_chacha-0.3.1/src/guts.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/run/media/stan/D/projects/rust/guessing_game/target/debug/guessing_game: /run/media/stan/D/projects/rust/guessing_game/src/main.rs
