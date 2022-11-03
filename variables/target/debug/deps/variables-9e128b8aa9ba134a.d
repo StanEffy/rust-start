8a475a9ed2228d807eb7b84ec2e6152c51f1006b
Stan Effy <holydonk@gmail.com>
2022-11-03 19:35:41 +0200

    new folder

---
+++ variables/target/debug/deps/variables-9e128b8aa9ba134a.d
@@ -0,0 +1,5 @@
+/run/media/stan/D/projects/rust/variables/target/debug/deps/variables-9e128b8aa9ba134a: src/main.rs
+
+/run/media/stan/D/projects/rust/variables/target/debug/deps/variables-9e128b8aa9ba134a.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/run/media/stan/D/projects/rust/variables/target/debug/variables: /run/media/stan/D/projects/rust/variables/src/main.rs
