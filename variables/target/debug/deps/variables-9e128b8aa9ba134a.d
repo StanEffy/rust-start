6548e4287fba77fafbec75e907812a37fc8a465a
Stan Effy <holydonk@gmail.com>
2022-11-04 15:52:06 +0200

    delete variables

--- variables/target/debug/deps/variables-9e128b8aa9ba134a.d
+++
@@ -1,5 +0,0 @@
-/run/media/stan/D/projects/rust/variables/target/debug/deps/variables-9e128b8aa9ba134a: src/main.rs
-
-/run/media/stan/D/projects/rust/variables/target/debug/deps/variables-9e128b8aa9ba134a.d: src/main.rs
-
-src/main.rs:
