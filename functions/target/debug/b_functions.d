fe34cb0fb97adb3c25df041befe62d3ed3ded594
Stan Effy <holydonk@gmail.com>
2022-11-04 17:04:04 +0200

    complete functions

---
+++ functions/target/debug/b_functions.d
@@ -0,0 +1,1 @@
+/run/media/stan/D/projects/rust/functions/target/debug/b_functions: /run/media/stan/D/projects/rust/functions/src/main.rs
---
+++ functions/target/debug/deps/b_functions-20e7657a7c99fd7b.d
@@ -0,0 +1,5 @@
+/run/media/stan/D/projects/rust/functions/target/debug/deps/b_functions-20e7657a7c99fd7b: src/main.rs
+
+/run/media/stan/D/projects/rust/functions/target/debug/deps/b_functions-20e7657a7c99fd7b.d: src/main.rs
+
+src/main.rs:
