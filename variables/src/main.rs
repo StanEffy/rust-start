23521e17910bf068ce51b7d98ecdccda021c927a
Stan Effy <holydonk@gmail.com>
2022-11-04 16:05:40 +0200

    refactor var initialization to a oneliner

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let mut missiles: u8 = 8;
-    let ready: u8 = 2;
+    let (mut missiles, ready) : (u32, u32) = (8, 2);
+
     println!("Firing {} of my {} missiles...", ready, missiles);
 
     missiles = missiles - ready;
