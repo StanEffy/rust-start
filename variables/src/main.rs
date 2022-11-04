7f728b4d84a1a5bc4e1b9e485fcf086445b418e9
Stan Effy <holydonk@gmail.com>
2022-11-04 16:03:30 +0200

    complete with constants

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
 
     missiles = missiles - ready;
     println!("{} missiles left", missiles);
-}
+
+    
+    println!("Firing {} of my {} missiles...", READY_AMOUNT, STARTING_MISSILES);
+}    
+
+    const STARTING_MISSILES: u128 = 8;
+    const READY_AMOUNT: u32 = 2;
+    
