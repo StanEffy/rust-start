3e83c83992f6a080fa00afb470e63bb9a6f90bf7
Stan Effy <holydonk@gmail.com>
2022-11-04 17:27:38 +0200

    3 ex for functions

---
+++ types/target/debug/deps/ding_machine-11fe170041a5fd96.d
@@ -0,0 +1,5 @@
+/run/media/stan/D/projects/rust/types/target/debug/deps/ding_machine-11fe170041a5fd96: src/main.rs
+
+/run/media/stan/D/projects/rust/types/target/debug/deps/ding_machine-11fe170041a5fd96.d: src/main.rs
+
+src/main.rs:
---
+++ types/target/debug/ding_machine.d
@@ -0,0 +1,1 @@
+/run/media/stan/D/projects/rust/types/target/debug/ding_machine: /run/media/stan/D/projects/rust/types/src/main.rs
