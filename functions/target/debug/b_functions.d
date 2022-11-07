aa4bb1de54b8ec74a57caa858059aa4ff79ee1b2
Stan Effy <holydonk@gmail.com>
2022-11-07 16:35:04 +0200

    clear from target

--- functions/target/debug/b_functions.d
+++
@@ -1,1 +0,0 @@
-/run/media/stan/D/projects/rust/functions/target/debug/b_functions: /run/media/stan/D/projects/rust/functions/src/main.rs
--- functions/target/debug/deps/b_functions-8d9f2a2df2811361.d
+++
@@ -1,5 +0,0 @@
-/run/media/stan/D/projects/rust/functions/target/debug/deps/b_functions-8d9f2a2df2811361.rmeta: src/main.rs
-
-/run/media/stan/D/projects/rust/functions/target/debug/deps/b_functions-8d9f2a2df2811361.d: src/main.rs
-
-src/main.rs:
